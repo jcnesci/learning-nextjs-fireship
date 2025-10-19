eee8e6f206bed55d831cd7e1a175fec4f8aeaa71
Jonathan Nesci <jnesci@JNesci-MBP14-2024.local>
2025-10-19 18:49:02 +0900

    Build an app>Static generation: use generateStaticParams to prerender data that doesn't change often.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,6 @@
+// Although we use generateStaticParams() we still need to refresh data once in a while, so revalidate it every 420 secs.
+export const revalidate = 420;
+
 interface Post {
   title: string;
   slug: string;
@@ -6,10 +9,19 @@ interface Post {
 interface Props {
   params: { slug: string };
 }
+
+// Pregenerate data that almost never changes (perf optimization)
+export async function generateStaticParams() {
+  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
+    (res) => res.json()
+  );
+  return posts.map((post) => ({ slug: post.slug }));
+}
+
 export default async function BlogPage({ params }: Props) {
-  const posts: Post[] = await fetch("http://localhost:3000/api/content", {
-    cache: "no-cache",
-  }).then((res) => res.json());
+  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
+    (res) => res.json()
+  );
   const post = posts.find((post) => post.slug === params.slug)!;
   return (
     <div>
