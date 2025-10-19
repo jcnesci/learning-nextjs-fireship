f2be2096d8764b5fdb464e9e0a22ea88eca61661
Jonathan Nesci <jnesci@JNesci-MBP14-2024.local>
2025-10-19 18:41:30 +0900

    Build an app>SSR: new blog page with dynamic path that uses server-side rendering (SSR) with caching options.

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,20 @@
+interface Post {
+  title: string;
+  slug: string;
+  content: string;
+}
+interface Props {
+  params: { slug: string };
+}
+export default async function BlogPage({ params }: Props) {
+  const posts: Post[] = await fetch("http://localhost:3000/api/content", {
+    cache: "no-cache",
+  }).then((res) => res.json());
+  const post = posts.find((post) => post.slug === params.slug)!;
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
