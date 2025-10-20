6341a7545aab31f841a0412197ac90c7fb909299
Jonathan Nesci <jnesci@JNesci-MBP14-2024.local>
2025-10-20 11:12:01 +0900

    User auth>Protect Routes: get session status on server-side to redirect to login on homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    // return <p>You must be signed in...</p>;
+    redirect("api/auth/signin");
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}></main>
