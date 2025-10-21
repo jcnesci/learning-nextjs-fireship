80d3f22403d4dd80f7dcddc287d0a4fb5b476f3e
Jonathan Nesci <jnesci@JNesci-MBP14-2024.local>
2025-10-21 10:02:26 +0900

    Advanced>Client Mutations: created the Follow btn to follow/unfollow user, uses server and client components.

--- components/FollowButton/FollowButton.tsx
+++ components/FollowButton/FollowButton.tsx
@@ -1,8 +1,7 @@
-import { getServerSession } from 'next-auth';
-import FollowClient from './FollowClient';
-import { prisma } from '@/lib/prisma';
-import { authOptions } from '../../app/api/auth/[...nextauth]/route'
-
+import { getServerSession } from "next-auth";
+import FollowClient from "./FollowClient";
+import { prisma } from "@/lib/prisma";
+import { authOptions } from "../../app/api/auth/[...nextauth]/route";
 
 interface Props {
   targetUserId: string;
